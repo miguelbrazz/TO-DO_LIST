e65a327c7037697379775b79d2f31eba62f4668a
unknown <msottom2@gmail.com>
2023-04-21 23:39:01 -0300

    falta customizar

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
-const inputBox = document.getElementById("input-box");
-const listContainer = document.getElementById("list-container");
+const inputField = document.querySelector(".input-field textarea"),
+todoLists = document.querySelector(".todoLists"),
+pendingNum = document.querySelector(".pending-num"),
+clearButton = document.querySelector(".clear-button");
 
-function addTask(){
-    if(inputBox.value === ''){
-        alert("You must write something..");
+function allTasks() {
+    let tasks = document.querySelectorAll(".pending");
+
+
+    pendingNum.textContent = tasks.length === 0? "no" : tasks.length;
+
+    let allLists = document.querySelectorAll(".list");
+    if (allLists.length > 0) {
+        todoLists.style.marginTop = "20px";
+        clearButton.style.pointerEvents = "auto";
+        return
     }
-    else{
-        let li = document.createElement("li");
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7"
-        li.appendChild(span);
-    }
-    inputBox.value = "";
+    todoLists.style.marginTop = "0px";
+    clearButton.style.pointerEvents = "none";
 }
 
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-    }
-}, false);
+inputField.addEventListener("keyup", (e) => {
+    let inputVal = inputField.value.trim();
 
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
+    if(e.key === "Enter" && inputVal.length > 0) {
+        let liTag = ` <li class="list pending" onclick="handleStatus(this)">
+        <input type="checkbox"/>
+        <span class="task">${inputVal}</span>
+        <i class="uil uil-trash-alt" onclick="deleteTask(this)"></i>
+       </li>`;
+
+       todoLists.insertAdjacentHTML("beforeend", liTag);
+       inputField.value = "";
+       allTasks();
+    }
+});
+
+function handleStatus(e) {
+    const checkbox = e.querySelector("input");
+    checkbox.checked = checkbox.checked ? false : true;
+    e.classList.toggle("pending");
+    allTasks();
 }
 
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask();
+function deleteTask(e) {
+    e.parentElement.remove();
+    allTasks();
+} 
+
+clearButton.addEventListener("click", () => {
+    todoLists.innerHTML = "";
+    allTasks();
+});
